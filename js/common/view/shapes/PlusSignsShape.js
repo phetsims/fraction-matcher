14984394fa307fd02ec452947fbd238f3f0da69c
Andrew Zelenkov <andrey.zelenkov@mlearner.com>
2013-11-29 16:02:14 +0400

    added pyramid and pluses shapes

--- js/common/view/shapes/PlusSignsShape.js
+++ js/common/view/shapes/PlusSignsShape.js
@@ -2,6 +2,7 @@
 
 /**
  * Plus sign view for 'Fraction Matcher' simulation.
+ * Only for fractions with denominator equal to 6.
  *
  * @author Andrey Zelenkov (Mlearner)
  */
@@ -10,26 +11,70 @@ define( function( require ) {
 
   // imports
   var inherit = require( "PHET_CORE/inherit" );
-  var Node = require( "SCENERY/nodes/Node" );
+  var AbstractShape = require( 'common/view/shapes/AbstractShape' );
   var Path = require( 'SCENERY/nodes/Path' );
   var Shape = require( 'KITE/Shape' );
 
   function PlusSignsShape( options ) {
-    options = _.extend( {
-        x: 0,
-        y: 0,
-        width: 100,
-        height: 100,
-        numerator: 1,  // numerator / denominator
-        denominator: 1, // numerator <= denominator
-        fill: "#F00",
-        freeFill: "#FFF",
-        stroke: "#000"
-      },
-      options );
-    var thisNode = this;
-    Node.call( thisNode, {x: options.x, y: options.y} );
+    var w,
+      h,
+      size,
+      denominator,
+      numerator,
+      pieces = [];
+
+    AbstractShape.call( this, options );
+    options = this.options;
+    denominator = options.denominator;
+    numerator = options.numerator;
+
+    // determine size of shape
+    w = options.width / 16;
+    h = options.height / 14;
+    size = Math.min( w, h );
+
+    // init arrays for shapes
+    for ( var i = 0, j; i < Math.ceil( numerator / denominator ); i++ ) {
+      pieces[i] = [];
+    }
+
+    // TODO: stroke
+    // create plus signs and add them to temporary array
+    for ( i = 0; i < pieces.length; i++ ) {
+      for ( j = 0; j < denominator / 2; j++ ) {
+        pieces[i].push( new Path( this.getPiece( size ), {
+          x: 4 * size * j, y: 2 * size * j, fill: 'white', stroke: options.stroke, lineWidth: 1
+        } ) );
+        pieces[i].push( new Path( this.getPiece( size ), {
+          x: (4 * j + 2) * size, y: (2 * j - 4) * size, fill: 'white', stroke: options.stroke, lineWidth: 1
+        } ) );
+      }
+    }
+
+    // add pluses to node
+    this.arrayToShapes( pieces, size );
+    this.setTranslation( -Math.max( 0, w - h ) - options.width / (9 / 2 - pieces.length), Math.max( 0, h - w ) / 2 );
   }
 
-  return inherit( Node, PlusSignsShape );
+  var plusSignPattern = [
+    {x: 1, y: 1},
+    {x: 3, y: 1},
+    {x: 3, y: -1},
+    {x: 1, y: -1},
+    {x: 1, y: -3},
+    {x: -1, y: -3},
+    {x: -1, y: -1},
+    {x: -3, y: -1},
+    {x: -3, y: 1},
+    {x: -1, y: 1},
+    {x: -1, y: 3},
+    {x: 1, y: 3},
+    {x: 1, y: 1}
+  ];
+
+  return inherit( AbstractShape, PlusSignsShape, {
+    getPiece: function( size ) {
+      return this.pointsToShape( new Shape(), plusSignPattern, size );
+    }
+  } );
 } );
