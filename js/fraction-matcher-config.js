624970a1476d11c2393d0f47b94e96e8fb9b7c80
Andrew Zelenkov <andrey.zelenkov@mlearner.com>
2013-12-19 12:11:05 +0400

    fixed path to common library

--- js/fraction-matcher-config.js
+++ js/fraction-matcher-config.js
@@ -42,7 +42,7 @@ require.config( {
 
     //Sim code
     FRACTION_MATCHER: '.',
-    FRACTION_COMMON: '../../fraction-common'
+    FRACTION_COMMON: '../../fraction-common/js'
   },
   // Configure the dependencies and exports for older, traditional 'browser globals' scripts
   // that do not use define() to declare the dependencies and set a module value.
