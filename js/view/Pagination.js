7be24a6fb265b9095cd25c47ee8b0954adbd090a
Andrew Zelenkov <andrey.zelenkov@mlearner.com>
2014-01-14 13:11:00 +0400

    new params pass to pagination constructor

--- js/view/Pagination.js
+++ js/view/Pagination.js
@@ -23,14 +23,14 @@ define( function( require ) {
 
     this.addChild( new PaginationNode( {x: 150, y: 90}, [
       [
-        {type: 'PIES', height: (mixedNumber ? 100 : null), numerator: mixedNumber ? 2 : 1, denominator: 1, value: 1, fill: model.CONSTANTS.COLORS.LIGHT_RED, label: StringUtils.format( patternLevelString, 1 )},
-        {type: 'HORIZONTAL_BARS', height: (mixedNumber ? 100 : null), numerator: mixedNumber ? 3 : 2, denominator: 2, value: 2, fill: model.CONSTANTS.COLORS.LIGHT_GREEN, label: StringUtils.format( patternLevelString, 2 )},
-        {type: 'VERTICAL_BARS', height: (mixedNumber ? 100 : null), numerator: mixedNumber ? 4 : 3, denominator: 3, value: 3, fill: model.CONSTANTS.COLORS.LIGHT_BLUE, label: StringUtils.format( patternLevelString, 3 )},
-        {type: 'LETTER_L_SHAPES', height: (mixedNumber ? 100 : null), numerator: mixedNumber ? 5 : 4, denominator: 4, value: 4, fill: model.CONSTANTS.COLORS.ORANGE, label: StringUtils.format( patternLevelString, 4 )},
-        {type: 'POLYGON', height: (mixedNumber ? 100 : null), numerator: mixedNumber ? 6 : 5, denominator: 5, value: 5, fill: model.CONSTANTS.COLORS.PINK, label: StringUtils.format( patternLevelString, 5 )},
-        {type: 'FLOWER', height: (mixedNumber ? 100 : null), numerator: mixedNumber ? 7 : 6, denominator: 6, value: 6, fill: model.CONSTANTS.COLORS.YELLOW, label: StringUtils.format( patternLevelString, 6 )},
-        {type: 'RING_OF_HEXAGONS', height: (mixedNumber ? 100 : null), numerator: mixedNumber ? 8 : 7, denominator: 7, value: 7, fill: model.CONSTANTS.COLORS.LIGHT_PINK, label: StringUtils.format( patternLevelString, 7 )},
-        {type: 'NINJA_STAR', height: (mixedNumber ? 100 : null), numerator: mixedNumber ? 9 : 8, denominator: 8, value: 8, fill: model.CONSTANTS.COLORS.GREEN, label: StringUtils.format( patternLevelString, 8 )}
+        {shape: {type: 'PIES', numerator: mixedNumber ? 2 : 1, denominator: 1, value: 1, fill: model.CONSTANTS.COLORS.LIGHT_RED}, height: (mixedNumber ? 100 : null), label: StringUtils.format( patternLevelString, 1 )},
+        {shape: {type: 'HORIZONTAL_BARS', numerator: mixedNumber ? 3 : 2, denominator: 2, value: 2, fill: model.CONSTANTS.COLORS.LIGHT_GREEN}, height: (mixedNumber ? 100 : null), label: StringUtils.format( patternLevelString, 2 )},
+        {shape: {type: 'VERTICAL_BARS', numerator: mixedNumber ? 4 : 3, denominator: 3, value: 3, fill: model.CONSTANTS.COLORS.LIGHT_BLUE}, height: (mixedNumber ? 100 : null), label: StringUtils.format( patternLevelString, 3 )},
+        {shape: {type: 'LETTER_L_SHAPES', numerator: mixedNumber ? 5 : 4, denominator: 4, value: 4, fill: model.CONSTANTS.COLORS.ORANGE}, height: (mixedNumber ? 100 : null), label: StringUtils.format( patternLevelString, 4 )},
+        {shape: {type: 'POLYGON', numerator: mixedNumber ? 6 : 5, denominator: 5, value: 5, fill: model.CONSTANTS.COLORS.PINK}, height: (mixedNumber ? 100 : null), label: StringUtils.format( patternLevelString, 5 )},
+        {shape: {type: 'FLOWER', numerator: mixedNumber ? 7 : 6, denominator: 6, value: 6, fill: model.CONSTANTS.COLORS.YELLOW}, height: (mixedNumber ? 100 : null), label: StringUtils.format( patternLevelString, 6 )},
+        {shape: {type: 'RING_OF_HEXAGONS', numerator: mixedNumber ? 8 : 7, denominator: 7, value: 7, fill: model.CONSTANTS.COLORS.LIGHT_PINK}, height: (mixedNumber ? 100 : null), label: StringUtils.format( patternLevelString, 7 )},
+        {shape: {type: 'NINJA_STAR', numerator: mixedNumber ? 9 : 8, denominator: 8, value: 8, fill: model.CONSTANTS.COLORS.GREEN}, height: (mixedNumber ? 100 : null), label: StringUtils.format( patternLevelString, 8 )}
       ]
     ], model.levelProperty, model.score ) );
 
