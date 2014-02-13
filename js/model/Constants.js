d3ecb6a5bcdcdf6a3860c64e05510afb753e0b4e
Vasily Shakhov <vasily.shakhov@mlearner.com>
2014-02-13 17:29:05 +0400

    pretty-print Constants.js

--- js/model/Constants.js
+++ js/model/Constants.js
@@ -239,143 +239,45 @@ define( function( require ) {
         shapes: this.SHAPES.slice( 0 )
       }
     ];
+
+    //mixed numbers added some more fractions or remove extra
     if ( game === mixedNumbersTitleString ) {
       // level 1
-      this.LEVEL_DESCRIPTION[0].fractions.pop(); // remove fraction 1/1
-      this.LEVEL_DESCRIPTION[0].fractions.push(  // add mixed fractions
-        [3, 2],
-        [4, 3]
-      );
+      // remove fraction 1/1
+      this.LEVEL_DESCRIPTION[0].fractions.pop();
+      // add mixed fractions
+      this.LEVEL_DESCRIPTION[0].fractions.push( [3, 2], [4, 3] );
 
       // level 2
-      this.LEVEL_DESCRIPTION[1].fractions.push(  // add more mixed fractions
-        [3, 2],
-        [4, 3],
-        [5, 3],
-        [5, 4],
-        [6, 4],
-        [6, 5]
-      );
+      // add more mixed fractions
+      this.LEVEL_DESCRIPTION[1].fractions.push( [3, 2], [4, 3], [5, 3], [5, 4], [6, 4], [6, 5] );
 
       // level 3
-      this.LEVEL_DESCRIPTION[2].fractions.push(  // add more mixed fractions
-        [5, 3],
-        [6, 5],
-        [7, 5],
-        [8, 5],
-        [9, 5],
-        [8, 6],
-        [9, 6],
-        [10, 6],
-        [11, 6],
-        [8, 7],
-        [9, 7],
-        [10, 7],
-        [11, 7],
-        [12, 7],
-        [13, 7],
-        [9, 8],
-        [10, 8],
-        [11, 8],
-        [12, 8],
-        [13, 8],
-        [14, 8],
-        [15, 8]
-      );
+      // add more mixed fractions
+      this.LEVEL_DESCRIPTION[2].fractions.push( [5, 3], [6, 5], [7, 5], [8, 5], [9, 5], [8, 6], [9, 6], [10, 6], [11, 6], [8, 7], [9, 7], [10, 7], [11, 7], [12, 7], [13, 7], [9, 8], [10, 8], [11, 8], [12, 8], [13, 8], [14, 8], [15, 8] );
 
       // level 4
-      this.LEVEL_DESCRIPTION[3].fractions.shift(); // remove one 13/7 fraction
-      this.LEVEL_DESCRIPTION[3].fractions.push(  // add more mixed fractions
-        [6, 5],
-        [7, 5],
-        [8, 5],
-        [7, 6],
-        [8, 6],
-        [9, 6],
-        [10, 6],
-        [11, 6],
-        [8, 7],
-        [10, 7],
-        [11, 7],
-        [12, 7],
-        [10, 8],
-        [11, 8],
-        [12, 8],
-        [13, 8],
-        [15, 8],
-        [10, 9],
-        [11, 9],
-        [12, 9],
-        [13, 9],
-        [14, 9],
-        [15, 9],
-        [16, 9],
-        [17, 9]
-      );
+      // remove one 13/7 fraction
+      this.LEVEL_DESCRIPTION[3].fractions.shift();
+      // add more mixed fractions
+      this.LEVEL_DESCRIPTION[3].fractions.push( [6, 5], [7, 5], [8, 5], [7, 6], [8, 6], [9, 6], [10, 6], [11, 6], [8, 7], [10, 7], [11, 7], [12, 7], [10, 8], [11, 8], [12, 8], [13, 8], [15, 8], [10, 9], [11, 9], [12, 9], [13, 9], [14, 9], [15, 9], [16, 9], [17, 9] );
       this.LEVEL_DESCRIPTION[3].numericScaleFactors = [1];
 
       // level 5
       this.LEVEL_DESCRIPTION[4].fractions = this.LEVEL_DESCRIPTION[3].fractions.slice( 0 );
 
       // level 6
-      this.LEVEL_DESCRIPTION[5].fractions.push( // add more mixed fractions
-        [10, 6],
-        [11, 6],
-        [8, 7],
-        [11, 7],
-        [12, 7],
-        [12, 8],
-        [13, 8],
-        [15, 8],
-        [12, 9],
-        [13, 9],
-        [14, 9],
-        [15, 9],
-        [16, 9],
-        [17, 9]
-      );
+      // add more mixed fractions
+      this.LEVEL_DESCRIPTION[5].fractions.push( [10, 6], [11, 6], [8, 7], [11, 7], [12, 7], [12, 8], [13, 8], [15, 8], [12, 9], [13, 9], [14, 9], [15, 9], [16, 9], [17, 9] );
 
       // level 7
-      this.LEVEL_DESCRIPTION[6].fractions.push( // add more mixed fractions
-        [8, 6],
-        [9, 6],
-        [10, 6],
-        [8, 7],
-        [9, 7],
-        [10, 7],
-        [11, 7],
-        [12, 7],
-        [13, 7],
-        [9, 8],
-        [10, 8],
-        [11, 8],
-        [12, 8],
-        [13, 8],
-        [14, 8],
-        [15, 8],
-        [10, 9],
-        [11, 9],
-        [12, 9],
-        [13, 9],
-        [14, 9],
-        [15, 9],
-        [16, 9],
-        [17, 9]
-      );
+      // add more mixed fractions
+      this.LEVEL_DESCRIPTION[6].fractions.push( [8, 6], [9, 6], [10, 6], [8, 7], [9, 7], [10, 7], [11, 7], [12, 7], [13, 7], [9, 8], [10, 8], [11, 8], [12, 8], [13, 8], [14, 8], [15, 8], [10, 9], [11, 9], [12, 9], [13, 9], [14, 9], [15, 9], [16, 9], [17, 9] );
       this.LEVEL_DESCRIPTION[6].numericScaleFactors = [3, 6, 7];
 
       // level 8
-      this.LEVEL_DESCRIPTION[7].fractions.push( // add more mixed fractions
-        [6, 5],
-        [7, 5],
-        [8, 5],
-        [9, 5],
-        [7, 6],
-        [8, 6],
-        [9, 6],
-        [10, 6],
-        [11, 6]
-      );
+      // add more mixed fractions
+      this.LEVEL_DESCRIPTION[7].fractions.push( [6, 5], [7, 5], [8, 5], [9, 5], [7, 6], [8, 6], [9, 6], [10, 6], [11, 6] );
       this.LEVEL_DESCRIPTION[7].numericScaleFactors = [3, 4, 5, 6, 7, 8, 9];
     }
   }
