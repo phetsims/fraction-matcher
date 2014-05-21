692fcaf0ea3fe77ec8815881dd2c78264da29c90
Sam Reid <reids@colorado.edu>
2014-05-20 18:32:43 -0600

    rewrote YellowButton to use RectangularPushButton, see https://github.com/phetsims/fraction-matcher/issues/9

--- js/yellow-buttons/YellowButton.js
+++ js/yellow-buttons/YellowButton.js
@@ -11,36 +11,19 @@ define( function( require ) {
 
   // modules
   var inherit = require( 'PHET_CORE/inherit' );
-  var Node = require( 'SCENERY/nodes/Node' );
-  var Rectangle = require( 'SCENERY/nodes/Rectangle' );
-  var PushButtonDeprecated = require( 'SUN/PushButtonDeprecated' );
+  var RectangularPushButton = require( 'SUN/buttons/RectangularPushButton' );
 
   function YellowButton( options ) {
-    var width = 70,
-      height = 35,
-      round = 4,
-      shadowOffset = {x: 3, y: 4},
-    // main view
-      base = new Node( {children: [new Rectangle( 0, 0, width, height, round, round, {fill: '#FEF452', stroke: 'black', lineWidth: 1} )]} ),
-    // shadow view
-      shadowBt = new Rectangle( shadowOffset.x, shadowOffset.y, width, height, round, round, {fill: '#CCC'} );
 
-    Node.call( this, options );
-
-    options.label.setTranslation( (width - options.labelWidth) / 2, (height - options.labelHeight) / 2 );
-    base.addChild( options.label );
-
-    // create push button
-    this.addChild( new PushButtonDeprecated(
-        new Node( {children: [shadowBt, base]} ),
-        new Node( {children: [shadowBt, base]} ),
-        new Node( {children: [base], x: shadowOffset.x, y: shadowOffset.y} ),
-        new Node( {children: [base]} ),
-        {listener: options.callback} )
-    );
-
-    this.scale( 0.75 );
+    RectangularPushButton.call( this, _.extend( {
+      content: options.label,
+      minWidth: 70,
+      minHeight: 35,
+      listener: options.callback,
+      baseColor: '#FEF452',
+      scale: 0.75
+    }, options ) );
   }
 
-  return inherit( Node, YellowButton );
+  return inherit( RectangularPushButton, YellowButton );
 } );
