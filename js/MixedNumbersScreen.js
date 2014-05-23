b010e553b887b5a4b51cbaea2467f869fcbbb3a5
Sam Reid <reids@colorado.edu>
2014-05-23 15:42:56 -0600

    Factored out screens to separate files

---
+++ js/MixedNumbersScreen.js
@@ -0,0 +1,44 @@
+// Copyright 2002-2014, University of Colorado Boulder
+
+/**
+ * Main entry point for the 'Fraction Matcher sim.
+ *
+ * @author Anton Ulyanov, Andrew Zelenkov (Mlearner)
+ */
+
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var SimLauncher = require( 'JOIST/SimLauncher' );
+  var Sim = require( 'JOIST/Sim' );
+  var Screen = require( 'JOIST/Screen' );
+  var Image = require( 'SCENERY/nodes/Image' );
+  var ScreenView = require( 'JOIST/ScreenView' );
+  var FractionMatcherModel = require( 'FRACTION_MATCHER/model/FractionMatcherModel' );
+  var FractionMatcherView = require( 'FRACTION_MATCHER/view/FractionMatcherView' );
+  var Constants = require( 'FRACTION_MATCHER/model/Constants' );
+  var MixedNumbersConstants = require( 'FRACTION_MATCHER/model/MixedNumbersConstants' );
+  var IntroIcon = require( 'FRACTION_MATCHER/view/IntroIcon' );
+  var MixedNumbersIcon = require( 'FRACTION_MATCHER/view/MixedNumbersIcon' );
+  var inherit = require( 'PHET_CORE/inherit' );
+
+  // strings
+  var mixedNumbersTitleString = require( 'string!FRACTION_MATCHER/mixedNumbersTitle' );
+  var matchingGameTitleString = require( 'string!FRACTION_MATCHER/introTitle' );
+  var simTitleString = require( 'string!FRACTION_MATCHER/simTitle' );
+
+  var simOptions = {
+    credits: '',
+    thanks: ''
+  };
+
+  function IntroScreen() {
+    Screen.call( this, mixedNumbersTitleString, new MixedNumbersIcon(),
+      function() { return new FractionMatcherModel( ScreenView.DEFAULT_LAYOUT_BOUNDS.width, ScreenView.DEFAULT_LAYOUT_BOUNDS.height, mixedNumbersTitleString, MixedNumbersConstants, true ); },
+      function( model ) { return new FractionMatcherView( model ); }
+    );
+  }
+
+  return inherit( Screen, IntroScreen );
+} );
--- js/fraction-matcher-main.js
+++ js/fraction-matcher-main.js
@@ -12,19 +12,10 @@ define( function( require ) {
   // modules
   var SimLauncher = require( 'JOIST/SimLauncher' );
   var Sim = require( 'JOIST/Sim' );
-  var Screen = require( 'JOIST/Screen' );
-  var Image = require( 'SCENERY/nodes/Image' );
-  var ScreenView = require( 'JOIST/ScreenView' );
-  var FractionMatcherModel = require( 'FRACTION_MATCHER/model/FractionMatcherModel' );
-  var FractionMatcherView = require( 'FRACTION_MATCHER/view/FractionMatcherView' );
-  var Constants = require( 'FRACTION_MATCHER/model/Constants' );
-  var MixedNumbersConstants = require( 'FRACTION_MATCHER/model/MixedNumbersConstants' );
-  var IntroIcon = require( 'FRACTION_MATCHER/view/IntroIcon' );
-  var MixedNumbersIcon = require( 'FRACTION_MATCHER/view/MixedNumbersIcon' );
+  var IntroScreen = require( 'FRACTION_MATCHER/IntroScreen' );
+  var MixedNumbersScreen = require( 'FRACTION_MATCHER/MixedNumbersScreen' );
 
   // strings
-  var mixedNumbersTitleString = require( 'string!FRACTION_MATCHER/mixedNumbersTitle' );
-  var matchingGameTitleString = require( 'string!FRACTION_MATCHER/introTitle' );
   var simTitleString = require( 'string!FRACTION_MATCHER/simTitle' );
 
   var simOptions = {
@@ -35,14 +26,8 @@ define( function( require ) {
   SimLauncher.launch( function() {
     // create and start the sim
     new Sim( simTitleString, [
-      new Screen( matchingGameTitleString, new IntroIcon(),
-        function() { return new FractionMatcherModel( ScreenView.DEFAULT_LAYOUT_BOUNDS.width, ScreenView.DEFAULT_LAYOUT_BOUNDS.height, matchingGameTitleString, Constants, false ); },
-        function( model ) { return new FractionMatcherView( model ); }
-      ),
-      new Screen( mixedNumbersTitleString, new MixedNumbersIcon(),
-        function() { return new FractionMatcherModel( ScreenView.DEFAULT_LAYOUT_BOUNDS.width, ScreenView.DEFAULT_LAYOUT_BOUNDS.height, mixedNumbersTitleString, MixedNumbersConstants, true ); },
-        function( model ) { return new FractionMatcherView( model ); }
-      )
+      new IntroScreen(),
+      new MixedNumbersScreen()
     ], simOptions ).start();
   } );
 } );
