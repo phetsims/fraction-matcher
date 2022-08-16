961f1099e10c0d2427cd7b44df2bcb79f6687541
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:38 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/fractionMatcherStrings.ts
+++ js/fractionMatcherStrings.ts
@@ -5,14 +5,18 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import fractionMatcher from './fractionMatcher.js';
 
 type StringsType = {
   'fraction-matcher': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'fractionsTitle': string;
+  'fractionsTitleProperty': TReadOnlyProperty<string>;
   'mixedNumbersTitle': string;
+  'mixedNumbersTitleProperty': TReadOnlyProperty<string>;
 };
 
 const fractionMatcherStrings = getStringModule( 'FRACTION_MATCHER' ) as StringsType;
