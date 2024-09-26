0fd5f9c61147d3f457b1f686fad499e72ff746f4
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:09:55 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for fraction-matcher.
+ * ESLint configuration for fraction-matcher.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
